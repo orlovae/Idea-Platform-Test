de606b3d608fb9d57af280de8ae6a736bc31c8f5
Alexander Orlov <79202875363@ya.ru>
2024-07-25 17:20:24 +0300

    up version kotlin to 2.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.ksp)
     alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
